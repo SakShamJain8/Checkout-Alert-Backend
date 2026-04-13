dd3f031ee82f7b3aa0699852a49d3269d7481826
vansheekajain <vansheekajain26@gmail.com>
2026-04-13 21:19:10 +0530

    fixed pinging time issue

--- checkoutalert/checkoutalert/src/main/java/com/checkoutalert/checkoutalert/service/PingSchedulerService.java
+++ checkoutalert/checkoutalert/src/main/java/com/checkoutalert/checkoutalert/service/PingSchedulerService.java
@@ -69,7 +69,7 @@ public class PingSchedulerService {
         return true;
     }
 
-    @Scheduled(fixedDelay = 45000) // runs every 45 seconds
+    @Scheduled(fixedDelay = 50000) // runs every 50 seconds
     public void pingAllEndpoints() {
         List<MonitoredEndpoint> endpoints = endpointRepo.findAllByActiveTrue();
         log.info("Pinging {} endpoints...", endpoints.size());
