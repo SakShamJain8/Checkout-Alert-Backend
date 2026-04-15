46a831182a334643ccc368573f07325278273a21
SakShamJain8 <sakshamjain4471@gmail.com>
2026-04-15 15:35:56 +0530

    fix: add missing @Autowired import to BaselineCacheService

--- checkoutalert/checkoutalert/src/main/java/com/checkoutalert/checkoutalert/service/BaselineCacheService.java
+++ checkoutalert/checkoutalert/src/main/java/com/checkoutalert/checkoutalert/service/BaselineCacheService.java
@@ -1,6 +1,7 @@
 package com.checkoutalert.checkoutalert.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
